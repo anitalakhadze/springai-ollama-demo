ab34a321776220227ed73ee0d19af549b9cfe480
anita <talakhadzeani@gmail.com>
2024-04-02 19:27:27 +0300

    Try running the test with a non-default model

--- src/test/java/com/anita/springaiollamademo/SpringaiOllamaDemoApplicationTests.java
+++ src/test/java/com/anita/springaiollamademo/SpringaiOllamaDemoApplicationTests.java
@@ -3,6 +3,8 @@ package com.anita.springaiollamademo;
 import org.junit.jupiter.api.Test;
 import org.springframework.ai.ollama.OllamaChatClient;
 import org.springframework.ai.ollama.api.OllamaApi;
+import org.springframework.ai.ollama.api.OllamaModel;
+import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -12,7 +14,15 @@ class SpringaiOllamaDemoApplicationTests {
 	void testOllama() {
 		OllamaApi chatApi = new OllamaApi("http://localhost:11434");
 		OllamaChatClient ollamaChatClient = new OllamaChatClient(chatApi);
-		String response = ollamaChatClient.call("Why is the sky blue?");
+
+		OllamaOptions options = OllamaOptions
+				.create()
+				.withModel(OllamaModel.ORCA_MINI.id());
+
+		String response = ollamaChatClient
+				.withDefaultOptions(options)
+				.call("Why is the sky blue?");
+
 		System.out.println(response);
 	}
 
